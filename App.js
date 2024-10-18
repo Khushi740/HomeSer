49b6b973c1273e4c13064cb53212b81e10310272
unknown <khushisingh74003@gmail.com>
2024-10-18 12:46:01 +0530

    added HomeScreen

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import Register from './App/Screens/RegisterScreen/Register';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './App/Screens/HomeScreen/HomeScreen';
+import DetailScreen from './App/Screens/DetailScreen/DetailScreen';
 
 // Define the stack navigator
 const Stack = createStackNavigator();
@@ -16,6 +17,7 @@ export default function App() {
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Register" component={Register} />
         <Stack.Screen name ="Home" component={HomeScreen} />
+        <Stack.Screen name={"Details"} component={DetailScreen} />
       </Stack.Navigator>
       <StatusBar style="auto" />
     </NavigationContainer>
--- App/Screens/HomeScreen/HomeScreen.jsx
+++ App/Screens/HomeScreen/HomeScreen.jsx
@@ -1,10 +1,75 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+// screens/HomeScreen.js
+import React from 'react';
+import { FlatList, Pressable, Text, StyleSheet,ScrollView } from 'react-native';
 
-export default function HomeScreen() {
+const HomeScreen = ({ navigation }) => {
+  const cardData = [
+    { id: '1', name: 'Painting ' },
+    { id: '2', name: 'cleaning' },
+    { id: '3', name: 'washing' },
+    { id: '4', name: 'gardening' },
+    { id: '5', name: 'repairing' },
+    { id: '5', name: 'repairing' },
+   
+  ];
+
+  const renderItem = ({ item }) => (
+    <Pressable
+      style={styles.card}
+      onPress={() => navigation.push('Details', { cardData: item.name })}
+    >
+      <Text style={styles.cardText}>{item.name}</Text>
+    </Pressable>
+  );
+  numColumns=2;
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  )
-}
+    
+        <FlatList
+      data={cardData}
+      
+      numColumns={numColumns}  
+      columnWrapperStyle={styles.row} 
+      renderItem={renderItem}
+      key={numColumns}
+      contentContainerStyle={styles.contentContainer}
+    />
+    
+    
+  );
+};
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+  },
+  row: {
+    justifyContent: 'space-between',  // Add spacing between cards in the row
+  },
+  card: {
+    
+    borderRadius: 10,
+    padding: 20,
+    width:151,
+    height:140,
+    marginBottom: 15,
+    borderWidth:3,
+    shadowOpacity:3,
+    shadowRadius:3 ,
+    borderRadius:20,
+    
+    
+    
+    
+  },
+  cardText: {
+    fontSize: 23,
+    fontWeight: 'bold',
+    color: '#333',
+    textAlign:'center',
+    paddingTop:23
+  },
+});
+
+export default HomeScreen;
